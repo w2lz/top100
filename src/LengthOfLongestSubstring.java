01dcbe066826b404d61723291f8b748534b5b498
王二愣子 <wang2lengzi@88.com>
2025-07-15 20:18:01 +0800

    add content

--- src/LengthOfLongestSubstring.java
+++ src/LengthOfLongestSubstring.java
@@ -1,2 +1,15 @@
 public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        int[] counts = new int[256];
+        int i = 0, j = 0, maxLen = 0;
+        for (; i < s.length(); i ++) {
+            for (; j < s.length(); j ++) {
+                if (counts[s.charAt(j)] != 0) break;
+                counts[s.charAt(j)]++;
+            }
+            maxLen = Math.max(maxLen, j - i);
+            counts[s.charAt(i)]--;
+        }
+        return maxLen;
+    }
 }
